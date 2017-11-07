30a94b257c16fe7eaf31deb2349908612e1b8507
Dariusz Mędryk <Dariusz Mdryk>
2017-11-07 22:01:39 +0100

    Dodaj pliki projektów.

---
+++ Hub/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Text;
+
+namespace Hub
+{
+    class Program
+    {
+        static string eventHubName = "darekhub";
+        static string connectionString = "Endpoint=sb://eventhubdarek.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XeJKqq3gA0Fs36qPxC47YqX/4B+81JSGzuyrZAjdLZc=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl-C to stop the sender process");
+            Console.WriteLine("Press Enter to start now");
+            Console.ReadLine();
+            SendingRandomMessages();
+        }
+
+        static void SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            while (true)
+            {
+                try
+                {
+                    var message = Guid.NewGuid().ToString() + " Darek z Data = " + DateTimeOffset.Now.ToString();
+                    Console.WriteLine("{0} > Sending message from DAREK: {1}", DateTime.Now, message);
+                    eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                }
+
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ SimpleEventProcessor/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SimpleEventProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=sb://eventhubdarek.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XeJKqq3gA0Fs36qPxC47YqX/4B+81JSGzuyrZAjdLZc=";
+            string eventHubName = "darekhub";
+            string storageAccountName = "storagedarek";
+            string storageAccountKey = "ePAtgqCTKJklEH9UHkTgkUYksG+sIRvx5aDWdu07qZIGN//METdHyb7zvHBMT3bWfT2ldbyGp2VUCnN8NbdTaw==";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
